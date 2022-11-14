a4ba06bce6964fcf558fd3910598bbc24135a2a5
Mads Tinko Madsen <lyderslev1@gmail.com>
2022-11-14 21:58:07 +0100

    Added redirect on button click to display dialog

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-file-upload',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class FileUploadComponent implements OnInit {
 
   constructor() { }
+
 
   ngOnInit(): void {
   }
