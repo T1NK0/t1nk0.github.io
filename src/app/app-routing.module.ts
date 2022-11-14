a02c5e5da1e7b6935d94c0801d6b11b33237d3de
Mads Tinko Madsen <lyderslev1@gmail.com>
2022-11-14 21:07:58 +0100

    adding form on button click

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dialog', component: DialogBoxComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
